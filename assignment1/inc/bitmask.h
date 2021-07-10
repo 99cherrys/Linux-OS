7e7026432efbcc8488e6c96d2471b47786feb2a4
99cherrys <74057916+99cherrys@users.noreply.github.com>
2021-07-10 12:14:16 +0100

    Add files via upload

---
+++ assignment1/inc/bitmask.h
@@ -0,0 +1,19 @@
+#ifndef __BITMASK_H
+#define __BITMASK_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void set_bit();
+
+void reset_bit();
+
+void flip_bit();
+
+void query_bit();
+
+void mask_main();
+
+#endif
---
+++ assignment1/inc/myutils.h
@@ -0,0 +1,19 @@
+#ifndef __MYUTILS_H
+#define __MYUTILS_H
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long int factorial(long int num);
+
+int isPrime(long int num);
+
+bool isPallindrome(long int num);
+
+bool isInteger( char *oct);
+
+void utils_main();
+
+#endif
